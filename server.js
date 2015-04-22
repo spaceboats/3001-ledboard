9b31c8d08950e4032ee09c39424829223a889b7c
Alex Gustafson <alexdgus@gmail.com>
2015-04-22 22:28:20 +0000

    Added /api/v1/fill endpoint

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
+var height = 16;
+var numBoards = 3;
+
 var express = require('express.io');
+var canvas = require('canvas');
+var board = require('rpi-rgb-led-matrix');
+
 app = express();
 app.http().io();
 
-app.get('/', function(req, res){
+app.use(function(req, res, next) {
+  res.sendJSON = function(obj) {
+    res.set({'Content-Type': 'application/json'});
+    res.status(200);
+    res.send(obj);
+  };
+  next();
+});
+
+app.get('/', function(req, res) {
   res.status(200);
   res.sendfile(__dirname + "/index.html");
 })
 
+app.post('/api/v1/fill', function(req, res) {
+  board.fill(255, 255, 255);
+  res.sendJSON('ok');
+})
+
+board.start(height, numBoards);
+
 app.listen(80);
-/*
-http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(80);
-console.log('Server running');
-*/
