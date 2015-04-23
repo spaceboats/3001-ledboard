cbae868e9943e52665f2baec989ae3ed80f353cd
Alex Gustafson <alexdgus@gmail.com>
2015-04-23 15:28:21 +0000

    Add /api/v1/insert endpoint
    Basic function for drawing strings can be accessed through insert.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var width = 96;
 var height = 16;
 var numBoards = 3;
 
@@ -24,6 +25,25 @@ app.use(function(req, res, next) {
   next();
 });
 
+function stringCanvas(displayString) {
+  var stringCanvas = new canvas(width, height);
+  var ctx = stringCanvas.getContext('2d');
+  ctx.font = "22px courier";
+
+  setInterval(function() {
+    var str = 'Senior Design';
+
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, width, 16);
+
+    ctx.fillStyle = "#00FF79";
+    ctx.fillText(str, 0, height);
+
+    board.drawCanvas(ctx, width, height);
+
+  }, 1000)
+}
+
 app.get('/', function(req, res) {
   res.status(200);
   res.sendfile(__dirname + "/index.html");
@@ -36,6 +56,18 @@ app.post('/api/v1/fill', function(req, res) {
   else
     board.fill(255, 255, 255);
   res.sendJSON('ok');
+});
+
+app.post('/api/v1/insert', function(req, res) {
+  console.log(req.body)
+  if (req.body.scene == "string") {
+    if (req.body.string) {
+      stringCanvas(req.body.string);
+      res.sendJSON('ok');
+    }
+  }
+  else
+    res.sendJSON('JSON Error', 400);
 });
 
 board.start(height, numBoards);
